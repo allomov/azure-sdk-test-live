47dd29dc1f769f120a73b49434259157f0e8ddb3
A.S. Lomoff <lomov.as@gmail.com>
2013-11-11 14:57:04 +0300

    try to create an image from blob

--- test_image_creation.rb
+++ test_image_creation.rb
@@ -3,7 +3,7 @@ require './config'
 
 VirtualMachineManager = Azure::VirtualMachineManagement::VirtualMachineManagementService
 
-image_file = 'tmp/image.jpg'
+image_file = './tmp/root.vhd'
 image_blob_name = "image-blob"
 image_container_name = "image-container"
 
@@ -11,27 +11,36 @@ image_container_name = "image-container"
 # Create an azure storage blob service object
 azure_blob_service = Azure::BlobService.new
 
+# azure_blob_service.delete_container(image_container_name)
+# puts "Container #{image_container_name} is deleted."
+
+
 # Create a container
 # TODO: add method to check if it container exists 
-container = azure_blob_service.create_container("image-container")
+container = azure_blob_service.create_container(image_container_name)
 
 # Upload a Blob
 # TODO: upload a file, not content
-content = File.open(image_file, 'rb') { |file| file.read }
-azure_blob_service.create_block_blob(container.name, image_blob_name, content)
+# content = File.open(image_file, 'rb') { |file| file.read }
+content_file = File.new(image_file)
+azure_blob_service.create_block_blob(container.name, image_blob_name, content_file)
 
-# List Containers
-puts "Containers List:"
-azure_blob_service.list_containers().each { |c| puts c.name }
 
-# List Blobs
-puts "Blobs List:"
-azure_blob_service.list_blobs(container.name).each { |b| puts b.name }
 
-# Delete a Blob
-azure_blob_service.delete_blob(container.name, image_blob_name)
-puts "Blob #{image_blob_name} is deleted."
 
-# Delete a Container
-azure_blob_service.delete_container(image_container_name)
-puts "Container #{image_container_name} is deleted."
+
+# # List Containers
+# puts "Containers List:"
+# azure_blob_service.list_containers().each { |c| puts c.name }
+
+# # List Blobs
+# puts "Blobs List:"
+# azure_blob_service.list_blobs(container.name).each { |b| puts b.name }
+
+# # Delete a Blob
+# azure_blob_service.delete_blob(container.name, image_blob_name)
+# puts "Blob #{image_blob_name} is deleted."
+
+# # Delete a Container
+# azure_blob_service.delete_container(image_container_name)
+# puts "Container #{image_container_name} is deleted."
